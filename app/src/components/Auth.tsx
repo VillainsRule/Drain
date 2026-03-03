c4bd827d84562c89e10c49ed49861284dd6780d4
thonk <hello@villainsrule.xyz>
2026-03-03 10:16:03 -0500

    rework login page

--- app/src/components/Auth.tsx
+++ app/src/components/Auth.tsx
@@ -5,7 +5,7 @@ import { observer } from 'mobx-react-lite'
 import { startAuthentication } from '@simplewebauthn/browser'
 
 import { Button } from '@/components/shadcn/button'
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/shadcn/card'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/shadcn/card'
 import { Input } from '@/components/shadcn/input'
 import { Label } from '@/components/shadcn/label'
 
@@ -64,8 +64,7 @@ const Auth = observer(function Auth() {
         <div className='min-h-screen flex items-center justify-center'>
             <Card className='w-11/12 md:w-full max-w-md'>
                 <CardHeader className='text-center flex flex-col items-center'>
-                    <CardTitle className='text-3xl font-bold'>Drain</CardTitle>
-                    <CardDescription>your credentials have been provided by the site admin.</CardDescription>
+                    <CardTitle className='text-4xl font-extrabold tracking-tight text-primary drop-shadow-sm'>Drain</CardTitle>
                 </CardHeader>
 
                 {showingAll ? <CardContent className='space-y-4'>
@@ -81,7 +80,7 @@ const Auth = observer(function Auth() {
 
                     {standardError && <div className='text-red-500 text-sm'>{standardError}</div>}
 
-                    <Button variant='outline' className='w-full cursor-pointer' ref={buttonRef} onClick={() => {
+                    <Button className='w-full cursor-pointer' ref={buttonRef} onClick={() => {
                         api.auth.account.post({
                             username: usernameRef.current!.value,
                             password: passwordRef.current!.value
@@ -93,38 +92,35 @@ const Auth = observer(function Auth() {
                         })
                     }}>Log In</Button>
 
-                    <div className='flex items-center'>
-                        <div className='grow h-px bg-ring' />
-                        <span className='mx-3 text-ring text-sm'>OR</span>
-                        <div className='grow h-px bg-ring' />
+                    <div className='flex w-full justify-center gap-3 items-center'>
+                        <Button variant='outline' className='flex-1 min-w-0 cursor-pointer' onClick={() => shadd.prompt(
+                            'have an invite code?',
+                            'enter the invite code to activate your account and get started with Drain!',
+                            { placeholder: 'invite code', maxLength: 36, minLength: 9 }, // old codes are 36L and new codes are 9L, so this should cover both
+                            async (value) => {
+                                const options = await api.auth.invites.attempt.post({ code: value });
+                                if (options.data) shadd.prompt(
+                                    `welcome, ${options.data.username}!`,
+                                    'get started by entering a password below:',
+                                    { placeholder: 'password', maxLength: 24, minLength: 3 },
+                                    async (value2) => {
+                                        const res = await api.auth.invites.claim.post({
+                                            code: value,
+                                            password: value2
+                                        });
+
+                                        if (res.data && res.data.user) {
+                                            authManager.setAuth(res.data.user);
+                                            location.reload();
+                                        } else shadd.setError(errorFrom(res));
+                                    }
+                                )
+                                else shadd.setError(errorFrom(options));
+                            }
+                        )}>i have an invite code</Button>
+
+                        {authManager.webAuthnEnabled && <Button variant='outline' className='flex-1 min-w-0 cursor-pointer' onClick={doWebAuthn}>i have a passkey</Button>}
                     </div>
-
-                    <Button variant='outline' className='w-full cursor-pointer' onClick={() => shadd.prompt(
-                        'have an invite code?',
-                        'enter the invite code to activate your account and get started with Drain!',
-                        { placeholder: 'invite code', maxLength: 36, minLength: 9 }, // old codes are 36L and new codes are 9L, so this should cover both
-                        async (value) => {
-                            const options = await api.auth.invites.attempt.post({ code: value });
-                            if (options.data) shadd.prompt(
-                                `welcome, ${options.data.username}!`,
-                                'get started by entering a password below:',
-                                { placeholder: 'password', maxLength: 24, minLength: 3 },
-                                async (value2) => {
-                                    const res = await api.auth.invites.claim.post({
-                                        code: value,
-                                        password: value2
-                                    });
-
-                                    if (res.data && res.data.user) {
-                                        authManager.setAuth(res.data.user);
-                                        location.reload();
-                                    } else shadd.setError(errorFrom(res));
-                                }
-                            )
-                            else shadd.setError(errorFrom(options));
-                        }
-                    )}>i have an invite code</Button>
-                    {authManager.webAuthnEnabled && <Button variant='outline' className='w-full cursor-pointer' onClick={doWebAuthn}>i have a passkey</Button>}
                 </CardContent> : <CardContent className='space-y-4'>
                     <div className='border-2 border-dashed bg-background p-6 text-center flex justify-center items-center w-full h-36 rounded-sm cursor-pointer hover:scale-101 transition-all duration-100' onClick={doWebAuthn}>
                         <span className='text-muted-foreground'>reauthenticate with your passkey</span>
