ec7ad358fe9564fefe040a4735b335fe70c06640
thonk <hello@villainsrule.xyz>
2026-03-08 21:18:04 -0400

    oopsies!

--- api/src/balancer/getProxy.ts
+++ api/src/balancer/getProxy.ts
@@ -1,18 +1,6 @@
 import configDB from '../db/impl/ConfigDB';
-import siteDB from '../db/impl/SiteDB';
 
-const getProxy = (): string | undefined => {
-    if (typeof Bun === 'undefined') return undefined;
-    if (!configDB.db.useProxiesForBalancer) return undefined;
-    
-    const site = siteDB.get('https.proxy');
-    if (!site) return undefined;
-
-    const keys = Object.keys(site.keys);
-    if (keys.length < 1) return undefined;
-
-    return keys[Math.floor(Math.random() * keys.length)];
-}
+const getProxy = (): string | undefined => (configDB.db.balancerProxy || undefined);
 
 const fetchWithProxy = async (url: string, options: BunFetchRequestInit = {}) => {
     const proxy = getProxy();
